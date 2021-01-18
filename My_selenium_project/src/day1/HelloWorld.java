94c3498ffa3750555eab2e8606640588d9ba3adc
Mohammad <mislam9981@yahoo.com>
2021-01-18 12:18:07 -0500

    my first upload

---
+++ My_selenium_project/src/day1/HelloWorld.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	
+		
+		System.out.println(" Assala mu Alikum");
+
+	}
+
+}
